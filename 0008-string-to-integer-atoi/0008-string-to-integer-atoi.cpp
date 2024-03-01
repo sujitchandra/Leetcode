c25d9e08912804903edd49271365804d11829ce7
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2024-03-01 18:01:30 +0530

    Time: 6 ms (24.28%), Space: 8.5 MB (9.95%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,36 +1,3 @@
-// class Solution {
-// public:
-//     int myAtoi(string s) {
-//         int n = s.size();
-//         if (n == 0) return 0;
-
-//         int i = 0;
-//         while (i < n && s[i] == ' ') i++; // checking for leading space..
-
-//         s = s.substr(i); //store everything after leading space
-
-//         int sign = +1;
-//         long long ans = 0;
-
-//         if (s[0] == '-') sign = -1; // for sign 
-
-//         int max_val = INT_MAX, min_val = INT_MIN;
-
-//         i = (s[0] == '+' || s[0] == '-') ? 1 : 0; // if 1st char is + or - start from 2nd char
-
-//         while (i < n) {
-//             if (s[i] == ' ' || !isdigit(s[i])) break; //if there is no digit
-
-//             ans = ans * 10 + s[i] - '0';
-
-//             if (sign == '+' && ans > max_val) return max_val;
-//             if (sign == '-' && -1 * ans < min_val) return min_val;
-
-//             i++;
-//         }
-//         return (int)(sign * ans);
-//     }
-// };
 
 class Solution {
     public :
@@ -38,23 +5,24 @@ class Solution {
         if(s.length()==0) return 0;
 
         int i=0;
-         while(i<s.size() && s[i]== ' ') {
+         while(i<s.size() && s[i]== ' ') { //checking for leading space
              i++;
          }     
-        s = s.substr(i); //i ---> last of string
+        s = s.substr(i); //going from i ---> last of string
         
         int sign = +1;
         long ans = 0;
         
-        if(s[0] == '-') sign = -1;
+        if(s[0] == '-') sign = -1; // for sign
         
         int MAX = INT_MAX, MIN = INT_MIN;
-        i = (s[0] == '+' || s[0] == '-') ? 1 : 0;
+        i = (s[0] == '+' || s[0] == '-') ? 1 : 0; // if 1st char is + or - start from 2nd char
 		
         while(i < s.length()) {
-            if(s[0] == ' ' || !isdigit(s[i])) break;
+            if(s[0] == ' ' || !isdigit(s[i])) break; //if there is no digit
             
             ans = ans * 10 + s[i]-'0';
+            
             if(sign == -1 && -1*ans < MIN) return MIN;
             if(sign == 1 && ans > MAX) return MAX;
             
