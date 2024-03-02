ca41439cc9a7fa0ea4d38a9d6814997ab7f805f6
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2024-03-02 12:52:56 +0530

    Time: 8 ms (67.20%), Space: 12.7 MB (30.22%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+         map<char,int> mpp;
+        mpp.insert({'I',1});
+        mpp.insert({'V',5});
+        mpp.insert({'X',10});
+        mpp.insert({'L',50});
+        mpp.insert({'C',100});
+        mpp.insert({'D',500});
+        mpp.insert({'M',1000});
+
+        int ans = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (i == s.size() - 1 || mpp[s[i]] >= mpp[s[i + 1]]) {
+                ans += mpp[s[i]];
+            } else {
+                ans -= mpp[s[i]];
+            }
+        }
+        return ans;
+    }
+};
