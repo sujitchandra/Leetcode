ef7a6b405f85ca917bd8aa1b6111be10898eea2c
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2024-04-07 13:40:33 +0530

    Time: 0 ms (100.00%), Space: 7 MB (85.21%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    long long power(long long n,long long t){
-        if(t == 0){
-            return 1;
-        }
-        if(t % 2 == 0){
-            long long key=power(n,t/2)%1000000007;
-            return (key%1000000007)*(key%1000000007);
-        }
-        long long key=power(n,t-1)%1000000007;
-        return (key*n)%1000000007;
+    unsigned long long power(unsigned long long n, unsigned long long t) {
+        if (t == 0) return 1;
+        unsigned long long temp = power(n, t/2);
+        if (t % 2 == 0)
+            return (temp * temp) % 1000000007;
+        else
+            return (n * temp * temp) % 1000000007;
     }
-    long long countGoodNumbers(long long n) {
-        long long even=5;
-        long long prime=4;
-        if(n % 2 == 0){
-            long long e=n/2;
-            return (power(5,e)%1000000007)*(power(4,e)%1000000007)%1000000007;
-        }
-        long long e=n/2;
-        return (power(5,e+1)%1000000007)*(power(4,e)%1000000007)%1000000007;
+
+    unsigned long long countGoodNumbers(unsigned long long n) {
+        unsigned long long even = (n + 1) / 2;
+        unsigned long long prime = n / 2;
+        unsigned long long first = power(5, even) % 1000000007;
+        unsigned long long second = power(4, prime) % 1000000007;
+        return (first * second) % 1000000007;
     }
 };
