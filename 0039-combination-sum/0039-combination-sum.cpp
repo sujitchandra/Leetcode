ec5e0b1a80c7ec0e47b626b7ee2279020c7622b8
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2024-04-10 17:45:47 +0530

    Time: 3 ms (82.03%), Space: 12.3 MB (65.42%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    void help(int ind, vector<int>& arr, int target, vector<int>& ds, vector<vector<int>>& ans){
+        
+        if(ind == arr.size()){
+            if(target == 0)
+                ans.push_back(ds);
+            return;
+        }
+        
+        // pick
+        
+        if(target >= arr[ind] ) {
+            ds.push_back(arr[ind]);
+            help(ind, arr, target - arr[ind], ds, ans);
+            ds.pop_back();
+        }
+        
+        // non-pick
+        
+        help(ind + 1, arr, target, ds, ans);
+    }    
+    
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> ds;
+        vector<vector<int>> ans;
+        help(0, candidates, target, ds, ans);
+        return ans;
+    }
+};
