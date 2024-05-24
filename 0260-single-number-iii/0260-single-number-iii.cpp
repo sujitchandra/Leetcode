cb0a52c12fafe3b2d138fb4a374ac50339b4d2bd
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2024-05-24 17:07:47 +0530

    Time: 3 ms (96.50%), Space: 12.2 MB (98.50%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int N = nums.size();
+        vector<int> ans;
+        long long xorr = 0;
+        for(int i=0; i<N; i++){
+            xorr ^= nums[i];
+        }
+        long long rightmost = (xorr & xorr-1) ^ xorr;
+        long long b1 = 0;
+        long long b2 = 0;
+        for(int i=0; i<N; i++){
+            if(nums[i] & rightmost) b1 = b1 ^ nums[i];
+            else 
+            b2 = b2 ^ nums[i];
+            
+            
+        }
+         ans.push_back(b1);
+         ans.push_back(b2);
+        if(ans[0] < ans[1]) {
+            swap(ans[0], ans[1]);
+        }
+         return ans;
+    }
+};
