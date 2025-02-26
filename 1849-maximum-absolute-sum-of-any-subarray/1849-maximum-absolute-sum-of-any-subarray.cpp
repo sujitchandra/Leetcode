a6277ad3c716b50ab47eb2cf641a7d0b1c2ae693
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-02-26 20:57:26 +0530

    Time: 0 ms (100%), Space: 45.1 MB (51.09%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
@@ -8,10 +8,17 @@ public:
         int res = 0; //[2,-5,1,-4,3,-2] , maxi = 3 , mini = -2
         for(int i=0; i<n; i++){
             presum += nums[i];
-            maxi = max(maxi , presum);
-            mini = min(mini , presum);
+            if(presum >= 0){
+                maxi = max(maxi, presum);
+                res = max(res, abs(presum - mini));
+                
+            }
+            else{
+                mini = min(mini , presum);
+                res  = max(res ,abs(presum - maxi));
+            }
         }
-        res = abs(maxi - mini);
+        // res = abs(maxi - mini);
         return res;
     }
 };
