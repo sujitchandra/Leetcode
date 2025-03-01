2fe8e1b167a680e3d05774add433c251ca731590
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-03-02 03:10:07 +0530

    Time: 6 ms (42.09%), Space: 18 MB (73.28%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,42 +1,28 @@
 // NOT UNDERSTAND.. FULLY..
-
 class Solution {
-private:
-    bool dfsCheck(int node, vector<int> adj[], vector<int>& vis, vector<int>& pathVis, vector<int>& topoOrder) {
-        vis[node] = 1;
-        pathVis[node] = 1;
-
-        for (auto it : adj[node]) {
-            if (!vis[it]) {
-                if (dfsCheck(it, adj, vis, pathVis, topoOrder))
-                    return true;
-            } else if (pathVis[it]) {
-                return true; // Cycle detected
-            }
-        }
-
-        pathVis[node] = 0;
-        topoOrder.push_back(node); // Add to topological order
-        return false;
-    }
-
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
-        for (auto& pre : prerequisites) {
-            adj[pre[1]].push_back(pre[0]);
+    vector<int> findOrder(int N, vector<vector<int>>& P) {
+        vector<vector<int>> G(N);                  
+        vector<int> ans, indegree(N);  
+
+        for(auto& pre : P) 
+            G[pre[1]].push_back(pre[0]),           
+            indegree[pre[0]]++;                     
+        
+        queue<int> q;
+        for(int i = 0; i < N; i++)
+            if(indegree[i] == 0) q.push(i);       
+        
+        while(size(q)) {
+            auto cur = q.front();
+             q.pop();
+            ans.push_back(cur);  
+                              
+            for(auto nextCourse : G[cur]) 
+                if(--indegree[nextCourse] == 0)     
+                    q.push(nextCourse);             
         }
-
-        vector<int> vis(numCourses, 0), pathVis(numCourses, 0), topoOrder;
-
-        for (int i = 0; i < numCourses; i++) {
-            if (!vis[i]) {
-                if (dfsCheck(i, adj, vis, pathVis, topoOrder))
-                    return {}; // Cycle detected, return empty list
-            }
-        }
-
-        reverse(topoOrder.begin(), topoOrder.end()); // Reverse for correct order
-        return topoOrder;
+        if(size(ans) == N) return ans;              
+        return {};                                      
     }
 };
