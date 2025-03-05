66796553f22daf6fa13ad1c088a27c3d50915c78
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-03-05 21:17:16 +0530

    Time: 0 ms (100%), Space: 8 MB (31.17%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long ans = 1 + 4LL * n *(n-1) /2;
-        return ans;
+        return 1 + 4LL * n *(n-1) /2;
+        
     }
 };
