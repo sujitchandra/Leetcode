d8609707eb0e3140f022ecddac88986c63df52c4
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-03-13 15:51:06 +0530

    Time: 14 ms (64.91%), Space: 17.2 MB (38.86%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -6,7 +6,10 @@ public:
 			dist[it[0]][it[1]] = it[2];
 			dist[it[1]][it[0]] = it[2];
 		}
+
 		for (int i = 0; i < n; i++) dist[i][i] = 0;
+
+
 		for (int k = 0; k < n; k++) {
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
