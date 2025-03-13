e62c0146cc6176148d4d6e66068b0d0b2c1d675f
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-03-13 15:48:46 +0530

    Time: 15 ms (60.39%), Space: 17 MB (53.09%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,46 +1,36 @@
 class Solution {
 public:
-    
-    int findTheCity(int n, vector<vector<int>>& edges, int dT) {
-        
-        vector<vector<int>>dist(n,vector<int>(n,1000000));
-        
-        for(int i=0;i<n;i++)
-            dist[i][i]=0;
-        
-        for(int i=0;i<edges.size();i++){
-            
-            int src=edges[i][0];
-            int dst=edges[i][1];
-            int wt=edges[i][2];
-            
-            dist[src][dst]=wt;
-            dist[dst][src]=wt;
-        }
-        
-        for(int k=0;k<n;k++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    dist[i][j]=min(dist[i][j],(dist[i][k]+dist[k][j]));
-                }
-            }
-        }
-        
-        
-        int min_city=0,max_val=n;
-        for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(i!=j && dist[i][j]<=dT){
-                    count++;
-                }
-            }
-            //cout<<count<<"\n";
-            if(count<=max_val){
-                min_city=i;
-                max_val=count;
-            }
-        }
-        return min_city;
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dist(n, vector<int> (n, INT_MAX));
+		for (auto it : edges) {
+			dist[it[0]][it[1]] = it[2];
+			dist[it[1]][it[0]] = it[2];
+		}
+		for (int i = 0; i < n; i++) dist[i][i] = 0;
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX)
+						continue;
+					dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+				}
+			}
+		}
+
+		int cntCity = n;
+		int cityNo = -1;
+		for (int city = 0; city < n; city++) {
+			int cnt = 0;
+			for (int adjCity = 0; adjCity < n; adjCity++) {
+				if (dist[city][adjCity] <= distanceThreshold)
+					cnt++;
+			}
+
+			if (cnt <= cntCity) {
+				cntCity = cnt;
+				cityNo = city;
+			}
+		}
+		return cityNo;
     }
 };
