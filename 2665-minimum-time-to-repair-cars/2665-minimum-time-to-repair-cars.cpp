17b7f17cec3e871059dd57b684ec6743acc63e9c
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-03-16 21:37:55 +0530

    Time: 56 ms (43.13%), Space: 59.3 MB (74.95%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    bool helper(vector<int>& ranks , int cars , long long mid){
+        long long totalcar = 0;
+            for(int rank:ranks){
+                totalcar += sqrt( mid/ rank);
+            }
+        return totalcar >= cars;
+    }
+    long long repairCars(vector<int>& ranks, int cars) { 
+        long long left = 1;
+        long long right = 1e14;
+        while(left < right){
+            long long mid = left + (right - left) /2;
+            if(helper(ranks , cars , mid)){
+                right = mid;
+            }
+           else{
+            left = mid + 1;
+           }
+        }
+        return left;
+    }
+};
