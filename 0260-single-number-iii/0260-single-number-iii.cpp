6ed19346ac1214e49a8817cfba7541c91876fa0f
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-03-17 20:04:16 +0530

    Time: 0 ms (100%), Space: 14.9 MB (15.83%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,26 +1,47 @@
+// class Solution {
+// public:
+//     vector<int> singleNumber(vector<int>& nums) {
+//         int N = nums.size();
+//         vector<int> ans;
+//         long long xorr = 0;
+//         for(int i=0; i<N; i++){
+//             xorr ^= nums[i];
+//         }
+//         long long rightmost = (xorr & xorr-1) ^ xorr;
+//         long long b1 = 0;
+//         long long b2 = 0;
+//         for(int i=0; i<N; i++){
+//             if(nums[i] & rightmost) b1 = b1 ^ nums[i];
+//             else 
+//             b2 = b2 ^ nums[i];
+            
+//         }
+//          ans.push_back(b1);
+//          ans.push_back(b2);
+//         // if(ans[0] < ans[1]) { // used for ascending order manner..
+//         //     swap(ans[0], ans[1]);
+//         // }
+//          return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int N = nums.size();
-        vector<int> ans;
-        long long xorr = 0;
-        for(int i=0; i<N; i++){
-            xorr ^= nums[i];
+        unordered_map<int, int> freq; // HashMap to store frequency
+        vector<int> result;
+
+        // Count occurrences of each number
+        for (int num : nums) {
+            freq[num]++;
         }
-        long long rightmost = (xorr & xorr-1) ^ xorr;
-        long long b1 = 0;
-        long long b2 = 0;
-        for(int i=0; i<N; i++){
-            if(nums[i] & rightmost) b1 = b1 ^ nums[i];
-            else 
-            b2 = b2 ^ nums[i];
-            
+
+        // Find the two numbers that appear exactly once
+        for (auto& pair : freq) {
+            if (pair.second == 1) {
+                result.push_back(pair.first);
+            }
         }
-         ans.push_back(b1);
-         ans.push_back(b2);
-        // if(ans[0] < ans[1]) {
-        //     swap(ans[0], ans[1]);
-        // }
-         return ans;
+
+        return result;
     }
 };
