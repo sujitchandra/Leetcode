157b56c94dad63372f7525515a50fb16f382513a
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-03-17 20:02:29 +0530

    Time: 0 ms (100%), Space: 13.9 MB (20.95%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ones = 0 , twos = 0;
-        for(int num: nums){
-            ones = (ones ^ num) & ~twos;
-            twos = (twos ^ num) & ~ones;
+        // int ones = 0 , twos = 0;
+        // for(int num: nums){
+        //     ones = (ones ^ num) & ~twos;
+        //     twos = (twos ^ num) & ~ones;
+        // }
+        // return ones;
+         unordered_map<int, int> freq; // HashMap to store frequency
+        
+        // Count occurrences of each number
+        for (int num : nums) {
+            freq[num]++;
         }
-        return ones;
+        
+        // Find the number that appears exactly once
+        for (auto& pair : freq) {
+            if (pair.second == 1) {
+                return pair.first;
+            }
+        }
+        
+        return -1;
     }
 };
