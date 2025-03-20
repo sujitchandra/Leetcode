aa92a0cc95e45aeb861e94845c6ae643db200bf6
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-03-20 21:11:02 +0530

    Time: 94 ms (52.27%), Space: 225.3 MB (52.27%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
@@ -1,47 +1,43 @@
 class Solution {
-    private:
-    int getcmpcost(int node, vector<vector<pair<int,int>>>&adj,vector<bool>&vis,vector<int>&cmp,int cid){
-        queue<int>q;
-        int ccost=INT_MAX;
-        q.push(node);
-        vis[node]=1;
-        while(!q.empty()){
-            int Node=q.front();
-            q.pop();
-            cmp[Node]=cid;
-            for(auto [nbr,w]:adj[Node]){
-                ccost&=w;
-                if(vis[nbr])continue;
-                vis[nbr]=1;
-                q.push(nbr);
-            }
-        }
-        return ccost;
-    }
 public:
     vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-        vector<vector<pair<int,int>>>adj(n);
-        for(auto ele:edges){
-            adj[ele[0]].push_back({ele[1],ele[2]});
-            adj[ele[1]].push_back({ele[0],ele[2]});
-        }
-        vector<bool>vis(n,0);
-        vector<int>cmp(n);
-        vector<int>cmpcost;
-        int cid=0;
-        for(int node=0;node<n;node++){
-            if(!vis[node]){
-                cmpcost.push_back(getcmpcost(node,adj,vis,cmp,cid));
-                cid++;
+        vector<int> arr(n, -1);
+        vector<int> val(n, -1);
+        for(auto edge: edges) {
+            int a = edge[0];
+            int b = edge[1];
+            while(arr[a] >= 0) a = arr[a];
+            while(arr[b] >= 0) b = arr[b];
+
+            int bit = edge[2];
+            if(val[a] != -1) bit &= val[a];
+            if(val[b] != -1) bit &= val[b];
+
+            if(a == b) {
+                val[a] = bit;
+            } else if(arr[a] <= arr[b]) {
+                arr[a] += arr[b];
+                arr[b] = a;
+                val[a] = bit;
+            } else {
+                arr[b] += arr[a];
+                arr[a] = b;
+                val[b] = bit;
             }
         }
-        vector<int>ans;
-        for(auto q:query){
-            int s=q[0];
-            int e=q[1];
-            if(cmp[s]==cmp[e])ans.push_back(cmpcost[cmp[s]]);
-            else ans.push_back(-1);
+
+        vector<int> res;
+        for(auto list: query) {
+            int a = list[0];
+            int b = list[1];
+            while(arr[a] >= 0) a = arr[a];
+            while(arr[b] >= 0) b = arr[b];
+            if(a==b)
+                res.push_back(val[a]);
+            else 
+                res.push_back(-1);
         }
-        return ans;
+        return res;
+        
     }
 };
