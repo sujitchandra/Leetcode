1bc7d2ce67070c3aabf029c90f038e41d530f112
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-03-24 22:58:18 +0530

    Time: 144 ms (16.5%), Space: 142.5 MB (16.5%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -20,19 +20,3 @@ public:
 };
 
 
-//  vector<bool>visited(days+1,false);
-//         int n = meetings.size();
-//         int m = meetings[0].size();
-
-//         for(int i=0; i<n; i++){
-//            for(int j= meetings[i][0]; j<=meetings[i][1]; j++){
-//                 visited[j] = true;
-//            }
-//         }
-//         int count = 0;
-//         for(int i=1; i<visited.size(); i++){
-//             if(visited[i] == false){
-//                 count++;
-//             }
-//         }
-//         return count;
