11d5fa0d61521f45d400095895242192ff399117
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-04-02 22:54:37 +0530

    Time: 3 ms (53.46%), Space: 12.1 MB (25.95%) - LeetHub

--- 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.cpp
+++ 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.cpp
@@ -1,15 +1,29 @@
+// class Solution {
+// public:
+//     int arithmeticTriplets(vector<int>& nums, int diff) {
+//          int n = nums.size();
+//         int cnt = 0;
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 for(int k=j+1; k<n; k++){
+//                     if((nums[j] - nums[i] == diff) && (nums[k] - nums[j] == diff)) cnt++;
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-         int n = nums.size();
-        int cnt = 0;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                for(int k=j+1; k<n; k++){
-                    if((nums[j] - nums[i] == diff) && (nums[k] - nums[j] == diff)) cnt++;
-                }
+        int counter = 0;
+        unordered_set <int> numSet (nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++){
+            if (numSet.count(nums[i] + diff) && numSet.count(nums[i] + (2*diff))){
+                counter ++;
             }
         }
-        return cnt;
+        return counter;
     }
 };
