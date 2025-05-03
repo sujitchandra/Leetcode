9b8981e6f74a815004334bd89ab3cb0a7165d546
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-05-04 00:45:04 +0530

    Time: 1 ms (77.8%), Space: 115.4 MB (76.51%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -1,25 +1,29 @@
-
 class Solution {
-public:
-    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int ans = INT_MAX;
-        for(int val=1; val<=6; val++){
-            int Swap1=0, Swap2=0;
+    int minDominoRotationsSubfuntion(int val, vector<int>& tops, vector<int>& bottoms) {
+        int ans_top = 0;
+        int ans_bottom = 0;
 
-            for(int i=0;i<tops.size();i++){
-                if(tops[i]!=val && bottoms[i]!=val){
-                    Swap1=INT_MAX;
-                    Swap2=INT_MAX; 
-                    break;
-                }
-
-                if(tops[i]!=val) Swap1++;
-                if(bottoms[i]!=val) Swap2++;
+        for (int i = 0; i < tops.size(); ++i) {
+            if (tops[i] != val && bottoms[i] != val) {
+                return -1;
+            } else if (tops[i] != val) {
+                ans_top++;
+            } else if (bottoms[i] != val) {
+                ans_bottom++;
             }
-
-            ans = min(ans, min(Swap1,Swap2));
         }
 
-        return ans==INT_MAX?-1:ans;
+        return std::min(ans_top, ans_bottom);
+    }
+
+public:
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int ans = minDominoRotationsSubfuntion(tops[0], tops, bottoms);
+
+        if (ans != -1 || tops[0] == bottoms[0]) {
+            return ans;
+        }
+
+        return minDominoRotationsSubfuntion(bottoms[0], bottoms, tops);
     }
 };
