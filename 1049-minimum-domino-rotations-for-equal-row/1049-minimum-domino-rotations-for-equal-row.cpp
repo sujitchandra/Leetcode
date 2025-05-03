f7fde0c2f7785292fb77fa14446edb0be6a0f17c
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-05-04 00:32:30 +0530

    Time: 8 ms (29.53%), Space: 115.5 MB (48.71%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -2,20 +2,24 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int mswaps=INT_MAX;
-        for(int val=1;val<=6;val++){
-            int tSwap=0, bSwap=0;
+        int ans = INT_MAX;
+        for(int val=1; val<=6; val++){
+            int Swap1=0, Swap2=0;
 
             for(int i=0;i<tops.size();i++){
-                if(tops[i]!=val && bottoms[i]!=val){tSwap=INT_MAX; bSwap=INT_MAX; break;}
+                if(tops[i]!=val && bottoms[i]!=val){
+                    Swap1=INT_MAX;
+                    Swap2=INT_MAX; 
+                    break;
+                }
 
-                if(tops[i]!=val){tSwap++;}
-                if(bottoms[i]!=val){bSwap++;}
+                if(tops[i]!=val){Swap1++;}
+                if(bottoms[i]!=val){Swap2++;}
             }
 
-            mswaps = min(mswaps, min(tSwap,bSwap));
+            ans = min(ans, min(Swap1,Swap2));
         }
 
-        return mswaps==INT_MAX?-1:mswaps;
+        return ans==INT_MAX?-1:ans;
     }
 };
