ab6189d6af7fde89ead660cb385fe5b1483959c9
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-05-23 18:49:38 +0530

    Time: 4 ms (48.39%), Space: 126.7 MB (49.68%) - LeetHub

--- 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.cpp
@@ -1,20 +1,18 @@
+//cp
 class Solution {
 public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        long long sum = 0;
-        vector<long long> res;
-        for (int x : nums) {
-            sum += x;
-            int y = x ^ k;
-            res.push_back(y - x);
+        long long res = 0;
+        int n = 0;
+        long long d = 1 << 30;
+        for(auto &x : nums){
+            long long y = x ^ k;
+            res += max((long long)x, y);
+            if(x < y){
+                n = 1 - n;
+            }
+            d = min(d, abs(x-y));
         }
-        sort(res.begin(), res.end());
-        reverse(res.begin(), res.end());
-        
-        for (int i = 0; i < res.size() - 1; i += 2) {
-            if (res[i] + res[i + 1] <= 0) break;
-            sum += res[i] + res[i + 1];
-        }
-        return sum;
+        return res - n * d;
     }
 };
