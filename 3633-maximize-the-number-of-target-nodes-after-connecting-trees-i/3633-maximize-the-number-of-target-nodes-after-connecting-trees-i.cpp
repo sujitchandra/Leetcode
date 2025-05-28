72cf19955deb0ea0604dd9b4d9ded67f1613e0fa
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-05-28 23:51:22 +0530

    Time: 475 ms (14.75%), Space: 205.1 MB (27.65%) - LeetHub

--- 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/3633-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/3633-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -1,29 +1,75 @@
+//CP
 class Solution {
 public:
-    vector<vector<int>> buildList(const vector<vector<int>>& edges) {
-        vector<vector<int>> adj(edges.size() + 1);
-        for (auto &e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-        }
-        return adj;
-    }
-    
-    int dfs(const vector<vector<int>>& adj, int u, int p, int k) {
-        if (k < 0) return 0;
-        int cnt = 1;
-        for (int v : adj[u])
-            if (v != p) cnt += dfs(adj, v, u, k-1);
-        return cnt;
-    }
-    
     vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
-        auto adj1 = buildList(edges1), adj2 = buildList(edges2);
-        int n = adj1.size(), m = adj2.size(), maxiB = 0;
-        vector<int> res(n);
+        int m=edges2.size();
+        vector<vector<int>>adj(m+1);
 
-        for (int i = 0; i < m; i++) maxiB = max(maxiB, dfs(adj2, i, -1, k - 1));
-        for (int i = 0; i < n; i++) res[i] = dfs(adj1, i, -1, k) + maxiB;
-        return res;
+        for(auto it : edges2){
+            int u=it[0];
+            int v=it[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        int maxi=0;
+        for(int i=0;i<=m;i++){
+            int cnt=0;
+            queue<pair<int,int>>q;
+            q.push({i,0});
+            vector<bool>vis(m+1,false);
+
+            while(!q.empty()){
+                auto it=q.front();
+                q.pop();
+                int node=it.first;
+                int step=it.second;
+                vis[node]=true;
+
+                if(step < k)
+                cnt++;
+
+                for(auto it : adj[node]){
+                    if(!vis[it] && step+1 < k)
+                    q.push({it,step+1});
+                }
+            }
+            maxi=max(maxi,cnt);
+        }
+        int n=edges1.size();
+        vector<vector<int>>adj1(n+1);
+
+        for(auto it : edges1){
+            int u=it[0];
+            int v=it[1];
+
+            adj1[u].push_back(v);
+            adj1[v].push_back(u);
+        }
+        vector<int>ans(n+1);
+        for(int i=0;i<=n;i++){
+            int cnt=0;
+            queue<pair<int,int>>q;
+            q.push({i,0});
+            vector<bool>vis(n+1,false);
+
+            while(!q.empty()){
+                auto it=q.front();
+                q.pop();
+                int node=it.first;
+                int step=it.second;
+                vis[node]=true;
+
+                if(step <= k)
+                cnt++;
+
+                for(auto it : adj1[node]){
+                    if(!vis[it] && step+1 <= k)
+                    q.push({it,step+1});
+                }
+            }
+            ans[i]=cnt+maxi;
+        }
+        return ans;
     }
 };
