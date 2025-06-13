f8227cf278fe1065a113b33a10863e4588905536
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-06-13 22:42:28 +0530

    Time: 22 ms (80.26%), Space: 86.9 MB (16.01%) - LeetHub

--- 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.cpp
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.cpp
@@ -4,19 +4,33 @@ class Solution {
 public:
     int minimizeMax(vector<int>& nums, int p) {
         if (p == 0) return 0;
+        
         sort(nums.begin(), nums.end());
-        int n = nums.size(), left = 0, right = nums[n - 1] - nums[0];
+        
+        int minThrehold = 0;
+        int maxThrehold = nums[nums.size() - 1] - nums[0];
 
-        while (left < right) {
-            int mid = left + (right - left) / 2, pairs = 0;
-            for (int i = 1; i < n; ++i) {
-                if (mid >= nums[i] - nums[i - 1]) {
-                    ++pairs, ++i;
+        while (minThrehold < maxThrehold) {
+            int candidateThrehold = minThrehold + (maxThrehold - minThrehold) / 2; // middle threshold
+            // check how many pairs we can pick with this threshold
+
+            int pairs = 0;
+
+            // greedily pick pairs
+            for (int i = 1; i < nums.size(); ++i) {
+                if (candidateThrehold >= nums[i] - nums[i - 1]) {
+                    ++pairs;
+                    ++i; // you have picked i and i -1, you can't pick [i + 1, i] pair anymore
                 }
             }
-            if (pairs >= p) right = mid;
-            else left = mid + 1;
+
+            if (pairs >= p) { // we are being to loose on the threshold and have too many pairs, be more strict on the threhold
+                maxThrehold = candidateThrehold;
+            } else { // we are being too strict about threshold and we do not have enough pairs, be more loose on threshold
+                minThrehold = candidateThrehold + 1;
+            }
         }
-        return left;
+
+        return minThrehold;
     }
 };
