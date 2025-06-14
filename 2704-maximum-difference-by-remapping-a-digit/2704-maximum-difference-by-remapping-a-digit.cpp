d5d5c01c4670a052074802cb9f4f908c5f512ef5
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-06-14 23:52:41 +0530

    Time: 0 ms (100%), Space: 8.2 MB (64.15%) - LeetHub

---
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string str = to_string(num);
+        int n = str.length();
+        int maxi = 0 , mini =0;
+        char change = '-';
+        int result = 0;
+        for(int i=0; i<n; i++){
+            maxi = str[i] - '0';
+            mini = str[i] -'0';
+            if(change == '-' && str[i] < '9') change = str[i];
+            if(change != '-' && str[i] == change) maxi = 9;
+            if(str[i] == str[0]) mini = 0;
+
+            result = result * 10 + maxi - mini;
+        }
+        return result;
+    }
+};
