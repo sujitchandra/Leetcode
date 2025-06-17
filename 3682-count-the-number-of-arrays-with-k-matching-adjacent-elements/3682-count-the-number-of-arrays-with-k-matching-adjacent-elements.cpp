d6820d632b270cf47d4d86b6c95bec56a0ed263e
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-06-17 23:48:08 +0530

    Time: 2 ms (90.91%), Space: 10.1 MB (78.41%) - LeetHub

--- 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
@@ -1,55 +1,44 @@
 //cp
 const int MOD = 1e9 + 7;
+const int MX = 1e5;
+
+long long fact[MX];
+long long inv_fact[MX];
 
 class Solution {
-public:
-    long long modInverse(long long a, long long mod){
+    long long qpow(long long x, int n) {
         long long res = 1;
-        long long power = mod - 2;
-
-        while (power){
-            if (power & 1)
-                res = res * a % mod;
-            a = a * a % mod;
-            power >>= 1;
+        while (n) {
+            if (n & 1) {
+                res = res * x % MOD;
+            }
+            x = x * x % MOD;
+            n >>= 1;
         }
-
         return res;
     }
 
-    int nCr(int n, int r){
-        if (r > n) return 0;
-        if (r == 0 || r == n) return 1;
-
-        long long res = 1;
-
-        for (int i = 1; i <= r; i++){
-            res = res * (n - r + i) % MOD;
-            res = res * modInverse(i, MOD) % MOD;
+    long long comb(int n, int m) {
+        return fact[n] * inv_fact[m] % MOD * inv_fact[n - m] % MOD;
+    }
+    void init() {
+        if (fact[0]) {
+            return;
+        }
+        fact[0] = 1;
+        for (int i = 1; i < MX; i++) {
+            fact[i] = fact[i - 1] * i % MOD;
         }
 
-        return (int)res;
-    }
-
-
-    long long bin_expo(long long a, long long b)
-    {
-        long long result = 1;
-        while (b) {
-            if (b & 1)
-                result = (result * a) % MOD;
-            a = (a * a) % MOD;
-            b >>= 1;
+        inv_fact[MX - 1] = qpow(fact[MX - 1], MOD - 2);
+        for (int i = MX - 1; i; i--) {
+            inv_fact[i - 1] = inv_fact[i] * i % MOD;
         }
-        return result;
     }
 
+public:
     int countGoodArrays(int n, int m, int k) {
-        // bin_expo = binary exponentiation = power a^b
-        int formula = (m * 1ll * bin_expo(m-1, n-(k+1))) % MOD;
-
-        int updated_formula = (formula *1ll* nCr(n-1, k)) % MOD; 
-
-        return updated_formula;
+        init();
+        return comb(n - 1, k) * m % MOD * qpow(m - 1, n - k - 1) % MOD;
     }
 };
