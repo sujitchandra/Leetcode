9503dede63aba47d1815d3d74548ea7aca8045b6
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-07-15 12:36:15 +0530

    Time: 2 ms (13.35%), Space: 8 MB (67.48%) - LeetHub

--- 3396-valid-word/3396-valid-word.cpp
+++ 3396-valid-word/3396-valid-word.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     bool isValid(string word) {
-        int n = word.size();
-        if (n < 3) return false;
-
-        int vowels = 0;
-        int consonants = 0;
-
-        for (char ch : word) {
-            if (!isalnum(ch)) return false;
-
-            if (isalpha(ch)) {
-                char lower = tolower(ch);
-                if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
-                    vowels++;
-                } else {
-                    consonants++;
+        int n=word.size();
+        if(n<3)
+        {
+            return false;
+        }
+        bool vowel=false;
+        bool consonant=false;
+        for(auto it:word)
+            {
+                if(!isalnum(it))
+                {
+                    return false;
+                }
+                if(isalpha(it))
+                {
+                    char ch=tolower(it);
+                    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                    {
+                        vowel=true;
+                    }
+                    else
+                    {
+                        consonant=true;
+                    }
                 }
             }
-        }
-        if (vowels > 0 && consonants > 0) return true;
-        else return false;
-
+        return vowel&&consonant;
     }
 };
