5a54e27a341ed417253ba6b49e7389c3361d00b9
Sujit  Chandra Das <109426898+sujitchandra@users.noreply.github.com>
2025-07-24 20:59:37 +0530

    Time: 28 ms (96.83%), Space: 19.3 MB (54.76%) - LeetHub

--- 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.cpp
+++ 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.cpp
@@ -1,74 +1,45 @@
 class Solution {
 public:
+    int calc(int part1, int part2, int part3) {
+        return max(part1, max(part2, part3)) - min(part1, min(part2, part3));
+    }
     int minimumScore(vector<int>& nums, vector<vector<int>>& edges) {
-        int n = nums.size();
-        vector<vector<int>> graph(n);
-        vector<unordered_set<int>> children(n);
-        vector<int> xor_val(nums);
-        vector<int> degree(n, 0);
-
-        for (const auto& e : edges) {
-            int u = e[0], v = e[1];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
-            degree[u]++;
-            degree[v]++;
+        int n = nums.size(), cnt = 0;
+        vector<int> sum(n), in(n), out(n);
+        vector<vector<int>> adj(n);
+        for (auto& e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
-
-        int total = 0;
-        queue<int> q;
-        vector<bool> seen(n, false);
-
-        for (int i = 0; i < n; ++i) {
-            total ^= nums[i];
-            if (degree[i] == 1) {
-                q.push(i);
-                seen[i] = true;
-            }
-        }
-
-        while (!q.empty()) {
-            int cur = q.front();
-            q.pop();
-            for (int next : graph[cur]) {
-                if (!seen[next]) {
-                    children[next].insert(cur);
-                    children[next].insert(children[cur].begin(), children[cur].end());
-                    xor_val[next] ^= xor_val[cur];
+        function<void(int, int)> dfs = [&](int x, int fa) {
+            in[x] = cnt++;
+            sum[x] = nums[x];
+            for (auto& y : adj[x]) {
+                if (y == fa) {
+                    continue;
                 }
-                degree[next]--;
-                if (degree[next] == 1 && !seen[next]) {
-                    seen[next] = true;
-                    q.push(next);
-                }
+                dfs(y, x);
+                sum[x] ^= sum[y];
             }
-        }
+            out[x] = cnt;
+        };
 
+        dfs(0, -1);
         int res = INT_MAX;
-        int m = edges.size();
-        for (int i = 0; i < m - 1; ++i) {
-            for (int j = i + 1; j < m; ++j) {
-                int a = edges[i][0], b = edges[i][1];
-                if (children[a].count(b)) swap(a, b);
-
-                int c = edges[j][0], d = edges[j][1];
-                if (children[c].count(d)) swap(c, d);
-
-                vector<int> vals;
-                if (children[a].count(c)) {
-                    vals = {xor_val[c], xor_val[a] ^ xor_val[c], total ^ xor_val[a]};
-                } else if (children[c].count(a)) {
-                    vals = {xor_val[a], xor_val[c] ^ xor_val[a], total ^ xor_val[c]};
+        for (int u = 1; u < n; u++) {
+            for (int v = u + 1; v < n; v++) {
+                if (in[v] > in[u] && in[v] < out[u]) {
+                    res = min(res,
+                              calc(sum[0] ^ sum[u], sum[u] ^ sum[v], sum[v]));
+                } else if (in[u] > in[v] && in[u] < out[v]) {
+                    res = min(res,
+                              calc(sum[0] ^ sum[v], sum[v] ^ sum[u], sum[u]));
                 } else {
-                    vals = {xor_val[a], xor_val[c], total ^ xor_val[a] ^ xor_val[c]};
+                    res = min(res,
+                              calc(sum[0] ^ sum[u] ^ sum[v], sum[u], sum[v]));
                 }
-
-                int max_v = *max_element(vals.begin(), vals.end());
-                int min_v = *min_element(vals.begin(), vals.end());
-                res = min(res, max_v - min_v);
             }
         }
-
         return res;
     }
 };
